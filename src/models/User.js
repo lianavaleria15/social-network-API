d84f372b180ac0e7a96ae6a9374ec2bc5ceeac24
liana.valeria15 <liana.valeria15@gmail.com>
2022-02-07 14:35:57 +0000

    created user schema

--- src/models/User.js
+++ src/models/User.js
@@ -1,17 +1,44 @@
 //import schema & model from mongoose
+const { Schema, model, Mongoose } = require("mongoose");
+
+//import mongoose validate email library
+const MongooseEmail = require("mongoose-type-email");
 
 //import thought model
+const thoughts = require("./Thought");
 
 //define user schema
+const userSchema = {
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+
+  email: {
+    type: MongooseEmail.SchemaTypes.Email,
+    required: true,
+    unique: true,
+  },
+
+  //thoughts (array of _id values referencing the though model)
+  thoughts: [thoughts],
+
+  //friends (array of _id values referencing the user model - self-reference)
+  friends: [
+    {
+      _id: String,
+    },
+  ],
+
+  //schema setting: create a virtual `friendCount` that retrieves user's friends array field length on query
+};
 
 //create new student schema
+const schema = new Schema(userSchema);
 
-/*username (string, unique, required, trimmed)
-  email (string, required, unique, must match a valid email address)
-  thoughts (array of _id values referencing the though model)
-  friends (array of _id values referencing the user model - self-reference)
-
-  schema setting: create a virtual `friendCount` that retrieves user's friends array field length on query
-  */
+const User = model("user", schema);
 
 //export user model
+module.exports = User;
