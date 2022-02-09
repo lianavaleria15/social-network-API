9b3a1a86be0e6805220a9d5a5dfb7e945fdf1270
liana.valeria15 <liana.valeria15@gmail.com>
2022-02-09 13:42:41 +0000

    added reaction seed file

---
+++ src/seeds/data/Reaction.js
@@ -0,0 +1,31 @@
+const reaction = [
+  {
+    reactionBody: "Tried 5 am club for a month and it changed my life",
+    username: "penny.finn",
+  },
+  {
+    reactionBody: "I am a night coding owl as well",
+    username: "fantastic_fox",
+  },
+  {
+    reactionBody: "No rest for the wicked mate",
+    username: "scott_vader97",
+  },
+  {
+    reactionBody: "Congrats Scott, well deserved",
+    username: "donna.travis05",
+  },
+  { reactionBody: "Well done mate! Beers on Sunday?", username: "josh.guru12" },
+  {
+    reactionBody: "Applied for 30 jobs, no responses..",
+    username: "donna.travis05",
+  },
+  {
+    reactionBody: "Try the link I sent you for cv tips",
+    username: "josh.guru12",
+  },
+  {
+    reactionBody: "Date-fns works with no issues, tested it",
+    username: "fantastic_fox",
+  },
+];
