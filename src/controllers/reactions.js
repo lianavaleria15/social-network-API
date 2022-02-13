fd630c9c4c81945460f4969a30c57879f9a6ab15
liana.valeria15 <liana.valeria15@gmail.com>
2022-02-13 11:55:42 +0000

    refactored add reaction controller

--- src/controllers/reactions.js
+++ src/controllers/reactions.js
@@ -3,23 +3,35 @@ const { Thought } = require("../models");
 
 //create a reaction stored in a single thought array
 const createReactionForThought = async (req, res) => {
-  //get reaction data from req body
-  const { reactionBody, username } = req.body;
+  try {
+    //get reaction data from req body
+    const { reactionBody, username } = req.body;
 
-  //get thought id from req params
-  const thoughtId = req.params;
+    //get thought id from req params
+    const thoughtId = req.params;
 
-  //get all thoughts from DB
-  const thoughtsFromDb = await Thought.find({});
+    //get all thoughts from DB
+    const thoughtsFromDb = await Thought.find({});
 
-  //find thought in thoughts array where id matches current id
-  const thought = thoughtsFromDb.find((thought) => (thought._id = thoughtId));
+    //find thought in thoughts array where id matches current id
+    const thought = thoughtsFromDb.find((thought) => (thought._id = thoughtId));
 
-  //push new reaction in reactions array for the current thought, this doesn't work
-  thought.reactions.push({ reactionBody, username });
+    //push new reaction in reactions array for the current thought, this doesn't work
+    thought.reactions.push({ reactionBody, username });
 
-  //push new reaction inside the reactions array for that thought id
-  await Thought.findByIdAndUpdate(thought._id, { ...thought });
+    //push new reaction inside the reactions array for that thought id
+    await Thought.findByIdAndUpdate(thought._id, { ...thought });
+
+    return res.json({ success: "New reaction successfully added" });
+  } catch (error) {
+    console.log(
+      `[ERROR]: Failed to create a new reaction for present thought | ${error.message}`
+    );
+    return res.status(500).json({
+      success: false,
+      error: "Failed to create a new reaction for present thought",
+    });
+  }
 };
 
 //delete a reaction from reactions array by the reaction id value
