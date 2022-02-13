76e55af0a9bd9f6accbf78b9e5d2625f5abbb70d
liana.valeria15 <liana.valeria15@gmail.com>
2022-02-13 11:47:24 +0000

    added controller for add reactions

--- src/controllers/reactions.js
+++ src/controllers/reactions.js
@@ -1,9 +1,25 @@
 //import reactions schema
-const { Reaction } = require("../models");
+const { Thought } = require("../models");
 
 //create a reaction stored in a single thought array
-const createReactionForThought = (req, res) => {
-  console.log("create reaction");
+const createReactionForThought = async (req, res) => {
+  //get reaction data from req body
+  const { reactionBody, username } = req.body;
+
+  //get thought id from req params
+  const thoughtId = req.params;
+
+  //get all thoughts from DB
+  const thoughtsFromDb = await Thought.find({});
+
+  //find thought in thoughts array where id matches current id
+  const thought = thoughtsFromDb.find((thought) => (thought._id = thoughtId));
+
+  //push new reaction in reactions array for the current thought, this doesn't work
+  thought.reactions.push({ reactionBody, username });
+
+  //push new reaction inside the reactions array for that thought id
+  await Thought.findByIdAndUpdate(thought._id, { ...thought });
 };
 
 //delete a reaction from reactions array by the reaction id value
